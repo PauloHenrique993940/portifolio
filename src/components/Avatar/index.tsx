ab5f2dd395d5454cb221b804a4390b1cb3a1b8ae
Paulo Henrique Ferreira França <paulohenriqueferreirafranca2@gmail.com>
2025-03-30 16:13:32 -0300

    subindo

--- src/components/Avatar/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FotoPerfil } from './styles';
-import React from 'react';
-
-const Avatar = () => (
-  <FotoPerfil  src="https://avatars.githubusercontent.com/u/162806228?v=4" />
-)
-
-export default Avatar;
